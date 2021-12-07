cb233dabac49e02b86e4409a4aaa6a5b72e7c1b9
vladzimir <vladen85@gmail.com>
2021-12-07 23:59:40 +0300

    added sql homework

---
+++ homeworkSQL.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+kit = ['элемент', 22, 'списка', 10, 'слово', 33]
+
+
+if __name__ == '__main__':
+
+    db = sqlite3.connect("domBase.db")
+    courser = db.cursor()
+
+    courser.execute('''
+    CREATE TABLE IF NOT EXISTS textTabl
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    texts TEXT);
+    ''')
+    db.commit()
+
+    courser.execute('''
+    CREATE TABLE IF NOT EXISTS numTabl
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    number INTEGER);
+    ''')
+    db.commit()
+
+    for cell in kit:
+        if type(cell) is str:
+            courser.execute('''INSERT INTO textTabl(texts) VALUES (?);''', (cell,))
+            db.commit()
+            num = int(len(cell))
+            courser.execute('''INSERT INTO numTabl(number) VALUES (?);''', (num,))
+            db.commit()
+        elif type(cell) is int:
+            if cell % 2 == 0:
+                courser.execute('''INSERT INTO numTabl(number) VALUES (?);''', (cell,))
+                db.commit()
+            else:
+                courser.execute('''INSERT INTO textTabl(texts) VALUES ('нечетное');''')
+                db.commit()
